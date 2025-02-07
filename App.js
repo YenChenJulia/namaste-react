c43e1c555feb7062124086759c9aa7a1222db05a
Julia <fairy750122@gmail.com>
2025-02-07 12:21:33 +0800

    react element/component/composition

--- App.js
+++ App.js
@@ -1,14 +1,27 @@
-// const heading = React.createElement("h1", {id: "heading"}, "hello world from react");
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "this is h1 tag"),
-    React.createElement("h2", {}, "this is h2 tag"),
-  ]),
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "this is h1 tag"),
-    React.createElement("h2", {}, "this is h2 tag"),
-  ]),
-]);
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+const heading = React.createElement("h1", { id: "heading" }, "Namaste React");
+// ReactElement
+const jsxHeading = <h1 id="heading">jsx heading</h1>;
+
+// ReactComponent
+const ReactComponent = () => (
+  <div className="container">
+    <h1>ReactComponent</h1>
+  </div>
+);
+
+// ReactComponent composition
+
+const Heading = () => <h1 id="heading">jsx heading</h1>;
+
+const ReactComposition = () => (
+  <div className="container">
+    <Heading />
+    <h1>ReactComponent</h1>
+  </div>
+);
+const root = createRoot(document.getElementById("root"));
+// root.render(jsxHeading);
+root.render(<ReactComposition />);
---
+++ README.md
@@ -0,0 +1,20 @@
+# Namaste React
+
+...
+# Parcel
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - Written in C++
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compressing
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - Support Older Browsers
+- Diagnostic
+- Error Handling
+- HTTPs
+- Tree Shaking - Remove unused code

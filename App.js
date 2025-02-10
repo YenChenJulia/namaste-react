5e013d4cc3dfc81dde7d3d718cac147e795b5030
Julia <fairy750122@gmail.com>
2025-02-10 11:55:05 +0800

    react props

--- App.js
+++ App.js
@@ -1,27 +1,66 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-const heading = React.createElement("h1", { id: "heading" }, "Namaste React");
-// ReactElement
-const jsxHeading = <h1 id="heading">jsx heading</h1>;
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://img.freepik.com/premium-vector/restaurant-logo-design-template_79169-56.jpg?w=360"
+          alt="logo img"
+        ></img>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>home</li>
+          <li>About us</li>
+          <li>Contact us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </header>
+  );
+};
 
-// ReactComponent
-const ReactComponent = () => (
-  <div className="container">
-    <h1>ReactComponent</h1>
-  </div>
-);
+const RestaurantCard = (props) => {
+	const {resName, cuisine} = props
+  return (
+    <div
+      className="res-card"
+      style={{
+        background: "#f0f0f0",
+      }}
+    >
+			<img className="res-logo" alt="res-logo" src="https://img.freepik.com/free-photo/chicken-skewers-with-onions-top-salad_1220-567.jpg?semt=ais_hybrid"></img>
+      <h3>{resName}</h3>
+			<h4>{cuisine}</h4>
+			<h4>4.4 stars</h4>
+			<h4>38 minutes</h4>
+    </div>
+  );
+};
 
-// ReactComponent composition
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">search</div>
+      <div className="res-container">
+        <RestaurantCard resName="Magana Food" cuisine="North Indian Food"/>
+        <RestaurantCard resName="KFC" cuisine="Burger, fast food"/>
+      </div>
+    </div>
+  );
+};
 
-const Heading = () => <h1 id="heading">jsx heading</h1>;
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
-const ReactComposition = () => (
-  <div className="container">
-    <Heading />
-    <h1>ReactComponent</h1>
-  </div>
-);
 const root = createRoot(document.getElementById("root"));
-// root.render(jsxHeading);
-root.render(<ReactComposition />);
+root.render(<AppLayout />);

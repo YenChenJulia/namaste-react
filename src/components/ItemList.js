3f0c83852a380cd0c795e5ab2252fdb14264c69a
Julia <fairy750122@gmail.com>
2025-03-06 09:35:28 +0800

    create accordion

---
+++ src/components/ItemList.js
@@ -0,0 +1,9 @@
+const ItemList = ({ accordion }) => {
+  return (
+    <div>
+      <h3 className="text-center">{accordion}</h3>
+    </div>
+  );
+};
+
+export default ItemList;
--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,18 +1,25 @@
 import Shimmer from "./Shimmer";
 import useRestaurantInfo from "../utils/useRestaurantInfo";
 import { useParams } from "react-router-dom";
+import ItemList from "./ItemList";
 
 const RestaurantMenu = () => {
-  const resName = useParams()
+  const resName = useParams();
   const resList = useRestaurantInfo(resName);
 
   if (resList === null) return <Shimmer />;
 
   const { name, order } = resList;
+  const accordions = Object.keys(resList.nutritions);
+
   return (
-    <div>
-      <h1>Restaurant Name: {name}</h1>
-      <div>{order}</div>
+    <div className="m-auto w-6/12 bg-gray-50">
+      <h1 className="text-lg font-bold text-center">Restaurant Name: {name}</h1>
+      <div className="text-center">{order}</div>
+
+      {accordions.map((c) => {
+        return <ItemList accordion={c} key={c} />;
+      })}
     </div>
   );
 };
